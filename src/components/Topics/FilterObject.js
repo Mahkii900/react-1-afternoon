a9788b53960bba6b1d480b239b36bae2038b5928
Mahkii900 <col.graff.battleschool@gmail.com>
2019-07-08 15:20:46 -0600

    second commit

--- src/components/Topics/FilterObject.js
+++ src/components/Topics/FilterObject.js
@@ -1,9 +1,31 @@
 import React, {Component} from 'react'
 
 class FilterObject extends Component {
+    constructor() {
+        this.state = {
+            unFilteredArray: [{name: 'Bob'}, {name: 'Joe'}, {name: 'Freddy-boy'}],
+            userInput: '',
+            filteredArray: []
+        }
+    }
+
+    handleChange(val) {
+        this.setState({userInput: val})
+    }
+
+    filterArray(input) {
+        
+    }
+    
     render() {
         return(
-            <p>FilterObject Component</p>
+            <div className="puzzleBox filterObjectPB">
+                <h4>Filter Object</h4>
+                <span className='puzzleText'>Original: </span>
+                <input className='inputLine' onChange={ (e) => this.handleChange(e.target.value)}></input>
+                <button className='confirmationButton' onClick={ () => this.filterArray(this.state.userInput)}> Filter</button>
+                <span className='resultsBox filterObjectRB'>Filtered: </span>
+            </div>
         )
     }
 }
