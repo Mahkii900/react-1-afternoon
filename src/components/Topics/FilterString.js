503cfd7926269d20ad2393f4fd1ffe7322dbd7e0
Mahkii900 <col.graff.battleschool@gmail.com>
2019-07-08 16:00:07 -0600

    second commit

--- src/components/Topics/FilterString.js
+++ src/components/Topics/FilterString.js
@@ -1,9 +1,35 @@
 import React, {Component} from 'react'
 
 class FilterString extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+            unFilteredArray: [], //Add Strings
+            userInput: '',
+            filteredArray: []
+        }
+    }
+
+    handleChange(val) {
+        this.setState({userInput: val})
+    }
+
+    filterStrings(input) {
+        let arr = this.unFilteredArray
+        let str = arr.filter((ele) => {ele.includes(input)})
+        this.setState({filteredArray: str})
+    }
+    
     render() {
         return(
-            <p>FilterString Component</p>
+            <div className='puzzleBox filterStringPB'>
+                <h4>Filter String</h4>
+                <span className='puzzleText'>Unfiltered: {this.unFilteredArray}</span>
+                <input className='inputLine' onChange={ (e) => {this.handleChange(e.target.value)}}></input>
+                <button className='confirmationButton' onClikc={ () => this.filterStrings(this.state.userInput)}></button>
+                <span className='resultsBox filterStringRB'>Filtered: {this.filteredArray}</span>
+            </div>
         )
     }
 }
